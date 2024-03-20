56729dce661def4ecb4bf0897be4af9792ef4f38
Ayush Nighoskar <ayunighoskar@gmail.com>
2024-03-20 23:07:34 +0530

    Updated code

--- background.js
+++ background.js
@@ -2,6 +2,6 @@ chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
       return {redirectUrl: "https://www.youtube.com/watch?v=LLIwKiQ362E"};
     },
-    {urls: ["*://www.instagram.com/*"]},
+    {urls: ["*:https://www.netflix.com/in/*"]},
     ["blocking"]
   );
