80b3f5428d1fdaa8be62d94c0f34e0acb3c150d9
Ayush Nighoskar <ayunighoskar@gmail.com>
2024-03-21 20:34:55 +0530

    deleted background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-      return {redirectUrl: "https://www.youtube.com/watch?v=LLIwKiQ362E"};
-    },
-    {urls: ["*:https://www.netflix.com/in/*"]},
-    ["blocking"]
-  );
